0212beb16caf6cba8b28f85283ec61371a087e69
gilsonsilven <gilsonsilven@gmail.com>
2023-11-09 18:05:16 -0300

    AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA

--- src/components/transacoes/TransactionModal.jsx
+++ src/components/transacoes/TransactionModal.jsx
@@ -16,12 +16,15 @@ import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { DatePicker } from '@mui/x-date-pickers/DatePicker';
 import 'dayjs/locale/pt-br';
+import { render } from 'react-dom'
+import dayjs from 'dayjs'
+
 
 // acertar espaçamento
 // checar se a fonte está certa
-// checkbox list, como essa checkbox list funciona?
-// procurar library pro calendário
-// perguntar o que fazer na parte de categorias
+
+
+
 // acertar formatação valor (olhar como é feito organizze)
 // os tamanhos estão certos?
 // ajustar formatação do modal todo
@@ -33,6 +36,7 @@ import 'dayjs/locale/pt-br';
 // da pra diminuir o tamanho do modal baseado no state do checkboxdropdown menu
 
 // transformar checkbox em componentes
+
 
 const hindTituloModal = Hind(
     {
@@ -55,39 +59,51 @@ const robotoTextCheckboxOp = Roboto(
     }
 )
 
-const CheckBoxOptions = ({showCheckBox}) => {
 
+
+
+const AccountOptions = () => {
+
+    // tá uma bagunça mas funciona
+
+    const [showAccountOptions, setShowAccountOptions] = useState(false)
+
+    const accountOptionsDropDown = () => {
+        setShowAccountOptions(!showAccountOptions)
+    }
+
+    // teste
+    const accountOptionsList = ['pão', 'farinha', 'leite', 'feijão'];
+
+    const [selectAccountOption, setSelectAccountOption] = useState('Conta')
+
+    const selectAccount = (accountOptionsList) => {
+        setSelectAccountOption(accountOptionsList)
+        accountOptionsDropDown()
+    }
+
+    
     return (
+        <>
+            <div className="w-[220px] h-[40px]">                    
+                <div className="w-[220px] h-[48px] left-0 top-[7px] absolute justify-center items-center inline-flex">
+                    <div onClick={accountOptionsDropDown} className="flex w-[268px] h-[48px] items-center justify-between border-2 rounded border-secondary-200 cursor-pointer p-[10px]">
+                        <span className={`text-base leading-normal ${selectAccountOption == 'Conta' ? 'text-[#a9a9a9]' : 'text-blue'}`}>{selectAccountOption}</span>
+                        <ArrowDown2 size="20px"/>
+                    </div>
+                </div>                        
+            </div>         
+            <div className={`absolute top-[60px] bg-white w-full rounded-md shadow-md z-10 ${showAccountOptions ? 'flex' : 'hidden'}`}>
+                <ul className="flex flex-col">
+                    {   
 
-        <div className={`w-[500px] h-[80px] flex flex-col justify-start gap-4 mt-[20px] mb-[32px] ${showCheckBox ? 'flex' : 'hidden'}`}>
-            <div className='checkbox-container'>
-                <input className='' type="checkbox" name="everyday"/>
-                <label className={`${robotoTextCheckboxOp.className} text-[14px] text-secondary-500`} />
-                <span className={`${robotoTextCheckboxOp.className} text-[14px] text-secondary-500 ml-5`}>Todos os dias</span>
+                        accountOptionsList.map((accountOptionsList, index) => (
+                            <li onClick={() => selectAccount(accountOptionsList)} key={index} className="cursor-pointer">{accountOptionsList}</li>
+                        ))
+                    }
+                </ul>
             </div>
-            <div className='checkbox-container'>
-                <input className='' type="checkbox" name="everyday"/>
-                <label className={`${robotoTextCheckboxOp.className} text-[14px] text-secondary-500`} />
-                <span className={`${robotoTextCheckboxOp.className} text-[14px] text-secondary-500 ml-5`}>Todas as semanas na quinta-feira</span>
-            </div>                    
-            <div className='checkbox-container'>
-                <input className='' type="checkbox" name="everyday"/>
-                <label className={`${robotoTextCheckboxOp.className} text-[14px] text-secondary-500`} />
-                <span className={`${robotoTextCheckboxOp.className} text-[14px] text-secondary-500 ml-5`}>Todos os meses no dia 07</span>
-            </div>                                      
-        </div>        
-    )
-
-}
-
-const AccountOptions = ({showAccountOptions}) => {
-
-    const AccountOptionsList = ['pão', 'farinha', 'leite', 'feijão'];
-
-    return (
-        <div className={`absolute top-[60px] bg-white w-full rounded-md shadow-md z-10 ${showAccountOptions ? 'flex' : 'hidden'}`}>
-            
-        </div>
+        </>
     )
 }
 
@@ -99,10 +115,12 @@ const TransactionModal = ({modalIsOpen, changeModalIsOpen}) => {
         setCheckBoxDropDownOptions(!checkBoxDropDownOptions)
     }    
 
-    const [showAccountOptions, setShowAccountOptions] = useState(false)
+    const [date, setDate] = useState(dayjs())
 
-    const accountOptionsDropDown = () => {
-        setShowAccountOptions(!showAccountOptions)
+    console.log(date)
+
+    const changeDate = (newDate) => {
+        setDate(newDate)
     }
 
     if(!modalIsOpen) {
@@ -113,30 +131,38 @@ const TransactionModal = ({modalIsOpen, changeModalIsOpen}) => {
 
         <div className="fixed inset-0 bg-black bg-opacity-25 flex justify-center items-center">
             <div className={`w-[540px] bg-white rounded-[20px] flex-col justify-start items-center inline-flex ${checkBoxDropDownOptions ? 'h-[520px]' : 'h-[380px]'}`}>
-                <div className="mt-2 w-[500px] h-[43px] flex-col justify-start items-start gap-5 inline-flex">
+                <div className="mt-2 w-[500px] h-[44px] flex-col justify-start items-start gap-5 inline-flex">
                     <p className={`mt-3 text-center text-secondary-500 text-xl ${hindTituloModal.className}`}>Nova receita</p>
                     <span className="w-[500px] h-[0px] border-b border-secondary-500"></span>
                 </div>
 
-                <div className="w-[500px] mt-[36px] h-14 justify-between items-end inline-flex">
-                    <div className="w-[240px] h-[40px] p-2 border rounded border-secondary-300">
+                <div className="w-[500px] mt-[36px] h-[56px] justify-between items-end inline-flex">
+                    <div className="w-[240px] h-[40px] pl-2 border-2 rounded border-secondary-200">
                         <input type="text" placeholder="Nome *" className="w-full h-full focus:outline-none" />
                     </div>
-                    <div className="w-[240px] h-[40px] p-2 border rounded border-secondary-300 justify-start items-center gap-2 flex">
+                    <div className="w-[240px] h-[40px] p-2 border-2 rounded border-secondary-200">
                         <input type="text" placeholder="R$ 0.00 *" className=" w-full text-slate-600 text-[16px] focus:outline-none" />
                     </div>
                 </div>
 
                 <div className="w-[500px] mt-[32px] justify-between items-end inline-flex">
-                    <div className="w-[328px] h-[40px] border rounded border-secondary-300 flex justify-between items-center cursor-pointer p-[10px]">
+                    <div className="w-[328px] h-[40px] border-2 rounded border-secondary-200 flex justify-between items-center cursor-pointer p-[10px]">
                         <span className="text-[#a9a9a9] text-base leading-normal" disabled>Categoria</span>
                         <ArrowDown2 size="20px"/>
                     </div>                
                     <div className="w-[150px] h-[40px] border rounded border-secondary-300 justify-center items-end inline-flex">
                         <div>
-                        <LocalizationProvider adapterLocale="pt-br" dateAdapter={AdapterDayjs}>
-                            <DatePicker slotProps={{ textField: { size: 'small' } }}/>
-                        </LocalizationProvider>
+                            <LocalizationProvider adapterLocale="pt-br" dateAdapter={AdapterDayjs}>
+                                <DatePicker value={date} onChange={changeDate} slotProps={{ textField: 
+                                    {  
+                                        size: 'small' ,
+                                        focused: false,
+                                        color: 'secondary'
+
+                                    } 
+                                
+                                }}/>
+                            </LocalizationProvider>
                         </div>
                     </div>                        
                 </div>
@@ -151,21 +177,29 @@ const TransactionModal = ({modalIsOpen, changeModalIsOpen}) => {
                     </div>
                     
                     <div className='flex relative justify-end items-end left-20'>
-                        <div className="w-[220px] h-[40px]">                    
-                            <div className="w-[220px] h-12 left-0 top-[7px] absolute justify-center items-center inline-flex">
-                                <div onClick={accountOptionsDropDown} className="flex w-[267px] h-12 items-center justify-between border rounded border-slate-900 cursor-pointer p-[10px]">
-                                    <span className="text-[#a9a9a9] text-base leading-normal">Conta</span>
-                                    <ArrowDown2 size="20px"/>
-                                </div>
-                            </div>                        
-                        </div> 
 
-                        <AccountOptions showAccountOptions={showAccountOptions}/>      
+                        <AccountOptions/>      
 
                     </div>              
                 </div>
 
-                <CheckBoxOptions showCheckBox={checkBoxDropDownOptions} />
+                <div className={`w-[500px] h-[80px] flex flex-col justify-start gap-4 mt-[20px] mb-[32px] ${checkBoxDropDownOptions ? 'flex' : 'hidden'}`}>
+                    <div className='checkbox-container'>
+                        <input className='' type="checkbox" name="everyday"/>
+                        <label className={`${robotoTextCheckboxOp.className} text-[14px] text-secondary-500`} />
+                        <span className={`${robotoTextCheckboxOp.className} text-[14px] text-secondary-500 ml-5`}>Todos os dias</span>
+                    </div>
+                    <div className='checkbox-container'>
+                        <input className='' type="checkbox" name="everyday"/>
+                        <label className={`${robotoTextCheckboxOp.className} text-[14px] text-secondary-500`} />
+                        <span className={`${robotoTextCheckboxOp.className} text-[14px] text-secondary-500 ml-5`}>Todas as semanas na quinta-feira</span>
+                    </div>                    
+                    <div className='checkbox-container'>
+                        <input className='' type="checkbox" name="everyday"/>
+                        <label className={`${robotoTextCheckboxOp.className} text-[14px] text-secondary-500`} />
+                        <span className={`${robotoTextCheckboxOp.className} text-[14px] text-secondary-500 ml-5`}>Todos os meses no dia {date.$D}</span>
+                    </div>                                      
+                </div>                
 
                 <div className="w-[507px] mt-[24px] h-10 justify-between items-center inline-flex">
                     <div className="w-[184px] h-10 px-8 py-[7px] bg-[#E8EEF4] rounded-[5px] justify-center items-center gap-2.5 flex cursor-pointer" onClick={() => changeModalIsOpen(false)}>
