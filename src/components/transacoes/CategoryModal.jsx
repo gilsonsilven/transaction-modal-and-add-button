f547b69e03b1e51374ab00b50942c7f40dfe5f5f
gilsonsilven <gilsonsilven@gmail.com>
2023-11-23 21:41:06 -0300

    commit 23/11

--- src/components/transacoes/CategoryModal.jsx
+++ src/components/transacoes/CategoryModal.jsx
@@ -36,7 +36,8 @@ import { useState } from 'react';
 import {
     GalleryAdd,
     Colorfilter,
-    ArrowRight
+    ArrowRight,
+    Truck
 } from 'iconsax-react'
 
 // deixar colorfilter icon colorido?
@@ -46,33 +47,63 @@ const CategoryModal = () => {
     const listaDeCores = ['bg-[#E12C00]', 'bg-[#EBE900]', 'bg-[#11D17E]', 'bg-[#004EEB]', 'bg-[#D422E1]', 'bg-[#EBE50B]', 
                             'bg-[#8DBE24]', 'bg-[#F76C4C]', 'bg-[#D12460]', 'bg-[#0095CD]', 'bg-[#006CC3]', 'bg-[#00E1BF]'];
 
-    const [colorOptions, setColorOptions] = useState('false')
+    const [colorOptions, setColorOptions] = useState(false)
 
     const showColorOptions = () => {
         setColorOptions(!colorOptions)
+        if(iconOptions != false) {
+            setIconOptions(false)
+        }
+    }
+
+    const listaDeIcones = ['<Colorfilter color="#000334"/>']
+
+    const iconHandler = () => {
+        const key = 24
+    }
+
+    const [iconOptions, setIconOptions] = useState(false)
+
+    const showIconOptions = () => {
+        setIconOptions(!iconOptions)
+        if(colorOptions != false) {
+            setColorOptions(false)
+        }
+        
     }
 
     return (
         <div className="fixed inset-0 bg-black bg-opacity-25 flex justify-center items-center">
-            
 
-            <div className={`w-[480px] bg-white rounded-[15px] ${colorOptions ? 'h-[220px]' : 'h-[120px]'} `}>
+            <div className={`w-[480px] bg-white rounded-[15px] ${colorOptions ? 'h-[200px]' : 'h-[100px]'}  ${iconOptions ? 'h-[200px]' : 'h-[100px]'}`}>
 
                 <div className=" h-[100px] flex justify-center items-center gap-[16px]">
                     <div className="w-[300px] h-[48px] border-2 rounded border-secondary-200">
                         <input type="text" placeholder="Nome da Categoria" className="w-full text-secondary-500 h-full text-[16px] focus:outline-none" />
-                    </div>                
-                    <GalleryAdd color="#000334"/>
+                    </div>
+                    <div onClick={showIconOptions} className="cursor-pointer">                
+                        <GalleryAdd color="#000334"/>
+                    </div>
                     <div onClick={showColorOptions} className="cursor-pointer">
                         <Colorfilter color="#000334"/>
                     </div>
                     <ArrowRight color="#000334"/>
                 </div>
-                <div className={`bg-white w-[480px] h-[100px] ${colorOptions ? '' : 'hidden'}`}>
-                    <div className="bg-black grid grid-cols-6 gap-[12px] place-items-center">
+                <div className={`bg-white w-[480px] h-[80px] ${colorOptions ? '' : 'hidden'}`}>
+                    <div className="bg-white grid grid-cols-6 gap-[12px] place-items-center">
                         {
                             listaDeCores.map((lista, index) => (
-                                <div className={`rounded-full ${lista} w-[32px] h-[32px]`}></div>
+                                <div className={`rounded-full ${lista} w-[32px] h-[32px]`} />
+                            ))
+                        }
+                    </div>
+                </div>
+                <div className={` bg-white w-[480px] h-[380px] ${iconOptions ? '' : 'hidden'}`}>
+                    <div className="grid grid-cols-6 gap-[12px] place-items-center">
+                        {
+                            listaDeIcones.map((lista, index) => (
+                                
+                                ''
                             ))
                         }
                     </div>
